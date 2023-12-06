8377dccf6a7cda4854c2176c73c8a5ed5bc78a5e
MKesenheimer <m.kesenheimer@gmx.net>
2023-12-06 22:26:04 +0100

    New project: reversing a ford car key.

--- video_sdl-test/video_sdl_test.py
+++ video_sdl-test/video_sdl_test.py
@@ -7,66 +7,27 @@
 # GNU Radio Python Flow Graph
 # Title: Not titled yet
 # Author: kesenheimer
-# GNU Radio version: 3.8.2.0
-
-from distutils.version import StrictVersion
-
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print("Warning: failed to XInitThreads()")
+# GNU Radio version: 3.10.8.0
 
 from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import gr
 from gnuradio.filter import firdes
+from gnuradio.fft import window
 import sys
 import signal
-from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 from gnuradio import video_sdl
 
-from gnuradio import qtgui
 
-class video_sdl_test(gr.top_block, Qt.QWidget):
+
+
+class video_sdl_test(gr.top_block):
 
     def __init__(self):
-        gr.top_block.__init__(self, "Not titled yet")
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("Not titled yet")
-        qtgui.util.check_set_qss()
-        try:
-            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
-
-        self.settings = Qt.QSettings("GNU Radio", "video_sdl_test")
-
-        try:
-            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-                self.restoreGeometry(self.settings.value("geometry").toByteArray())
-            else:
-                self.restoreGeometry(self.settings.value("geometry"))
-        except:
-            pass
+        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
 
         ##################################################
         # Variables
@@ -76,12 +37,12 @@ class video_sdl_test(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
-        self.video_sdl_sink_0 = video_sdl.sink_s(0, 640, 480, 0, 640, 480)
-        self.blocks_throttle_1 = blocks.throttle(gr.sizeof_short*1, samp_rate,True)
+
+        self.video_sdl_sink_0 = video_sdl.sink_s(0, 640, 480, 640, 480)
+        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_short*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
         self.blocks_add_xx_0 = blocks.add_vss(1)
         self.analog_sig_source_x_2 = analog.sig_source_s(samp_rate, analog.GR_TRI_WAVE, 1234.5, 255, 0, 0)
         self.analog_sig_source_x_1 = analog.sig_source_s(samp_rate, analog.GR_COS_WAVE, 10000, 255, 0, 0)
-
 
 
         ##################################################
@@ -89,14 +50,9 @@ class video_sdl_test(gr.top_block, Qt.QWidget):
         ##################################################
         self.connect((self.analog_sig_source_x_1, 0), (self.blocks_add_xx_0, 0))
         self.connect((self.analog_sig_source_x_2, 0), (self.blocks_add_xx_0, 1))
-        self.connect((self.blocks_add_xx_0, 0), (self.blocks_throttle_1, 0))
-        self.connect((self.blocks_throttle_1, 0), (self.video_sdl_sink_0, 0))
+        self.connect((self.blocks_add_xx_0, 0), (self.blocks_throttle2_0, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self.video_sdl_sink_0, 0))
 
-
-    def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "video_sdl_test")
-        self.settings.setValue("geometry", self.saveGeometry())
-        event.accept()
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -105,43 +61,34 @@ class video_sdl_test(gr.top_block, Qt.QWidget):
         self.samp_rate = samp_rate
         self.analog_sig_source_x_1.set_sampling_freq(self.samp_rate)
         self.analog_sig_source_x_2.set_sampling_freq(self.samp_rate)
-        self.blocks_throttle_1.set_sample_rate(self.samp_rate)
-
+        self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
 
 
 
 
 def main(top_block_cls=video_sdl_test, options=None):
     if gr.enable_realtime_scheduling() != gr.RT_OK:
-        print("Error: failed to enable real-time scheduling.")
-
-    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
-    qapp = Qt.QApplication(sys.argv)
-
+        gr.logger("realtime").warning("Error: failed to enable real-time scheduling.")
     tb = top_block_cls()
 
-    tb.start()
-
-    tb.show()
-
     def sig_handler(sig=None, frame=None):
-        Qt.QApplication.quit()
+        tb.stop()
+        tb.wait()
+
+        sys.exit(0)
 
     signal.signal(signal.SIGINT, sig_handler)
     signal.signal(signal.SIGTERM, sig_handler)
 
-    timer = Qt.QTimer()
-    timer.start(500)
-    timer.timeout.connect(lambda: None)
+    tb.start()
 
-    def quitting():
-        tb.stop()
-        tb.wait()
+    try:
+        input('Press Enter to quit: ')
+    except EOFError:
+        pass
+    tb.stop()
+    tb.wait()
 
-    qapp.aboutToQuit.connect(quitting)
-    qapp.exec_()
 
 if __name__ == '__main__':
     main()
--- video_sdl-test/video_sdl_test/video_sdl_test.hpp
+++ video_sdl-test/video_sdl_test/video_sdl_test.hpp
@@ -5,7 +5,7 @@ GNU Radio C++ Flow Graph Header File
 
 Title: Not titled yet
 Author: kesenheimer
-GNU Radio version: 3.8.2.0
+GNU Radio version: 3.10.8.0
 ********************/
 
 /********************
@@ -15,21 +15,45 @@ GNU Radio version: 3.8.2.0
 #include <gnuradio/analog/sig_source.h>
 #include <gnuradio/blocks/add_blk.h>
 #include <gnuradio/blocks/throttle.h>
+#include <string_view>
+#include <algorithm>
 #include <gnuradio/video_sdl/sink_s.h>
 
+#include <QVBoxLayout>
+#include <QScrollArea>
+#include <QWidget>
+#include <QGridLayout>
+#include <QSettings>
+#include <QApplication>
 
 
 using namespace gr;
 
 
 
-class video_sdl_test {
+class video_sdl_test : public QWidget {
+    Q_OBJECT
 
 private:
+    QVBoxLayout *top_scroll_layout;
+    QScrollArea *top_scroll;
+    QWidget *top_widget;
+    QVBoxLayout *top_layout;
+    QGridLayout *top_grid_layout;
+    QSettings *settings;
 
 
     video_sdl::sink_s::sptr video_sdl_sink_0;
-    blocks::throttle::sptr blocks_throttle_1;
+    constexpr unsigned int blocks_throttle2_0_limit(float value) {
+      const std::string_view limited("auto");
+      if(limited == "time") {
+        return std::max(static_cast<unsigned int>(value * samp_rate), 1U);
+      } else if (limited == "items") {
+        return std::max(static_cast<unsigned int>(value), 1U);
+      }
+      return 0;
+    }
+    blocks::throttle::sptr blocks_throttle2_0;
     blocks::add_ss::sptr blocks_add_xx_0;
     analog::sig_source_s::sptr analog_sig_source_x_2;
     analog::sig_source_s::sptr analog_sig_source_x_1;
